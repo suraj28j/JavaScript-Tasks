74fdc453a3aad3d203f0603e3e9a74fdf594d3da
Suraj Kumar <suraj28j@gmail.com>
2024-11-21 19:43:18 +0530

    debouning update

--- Debouncing/script.js
+++ Debouncing/script.js
@@ -1,38 +1,70 @@
 const users = document.querySelector('.users');
-let data;
+let data = [];
 
 const getData = async () => {
     try {
         const res = await fetch(`https://api.github.com/users`);
         data = await res.json();
-        users.innerHTML = '';
-        data.forEach((item) => {
-            const div = document.createElement('div');
-            div.setAttribute("id", "card")
-            div.innerHTML = `
-            <img src = "${item.avatar_url}" alt = ${item.login}/>
-            <div>
-                <h2>${item.login}</h2>
-                <p><a href = "${item.html_url}">${item.html_url}</a></p>
-            </div>
-            `
-            users.appendChild(div);
-        })
-        // console.log(result);
+        displayData(data);
     } catch (error) {
         console.log(error);
     }
 }
-
 getData();
 
+const displayData = (data) => {
+    // console.log(data);
+    users.innerHTML = '';
+    data.forEach((item) => {
+        const div = document.createElement('div');
+        div.setAttribute("id", "card")
+        div.innerHTML = `
+        <img src = "${item.avatar_url}" alt = ${item.login}/>
+        <div>
+            <h2>${item.login}</h2>
+            <p><a href = "${item.html_url}">${item.html_url}</a></p>
+        </div>
+        `
+        users.appendChild(div);
+    })
+}
+
+
+
+
+const debounce = (func, wait) => {
+    let timerId;
+    return (e) => {
+        clearTimeout(timerId);
+        timerId = setTimeout(() => func(e), wait)
+    }
+}
+
+const callAPI = (e) => {
+    let filterData;
+    let inputData = e.target.value
+    console.log(inputData);
+    filterData = data.filter((item) => {
+        return item.login.includes(inputData);
+    })
+    displayData(filterData);
+}
+
+const debounceCallAPI = debounce(callAPI, 500);
 
 const input = document.querySelector("input");
-input.addEventListener('input', (e) => {
-    let inputData = e.target.value
-    // console.log(inputData);
-    data = data.filter((item) => {
-        return item.login === inputData
-    })
-    getData();
-})
+input.addEventListener('input', debounceCallAPI)
+
+
+
+// Normal Search //
+
+// input.addEventListener('input', (e) => {
+    // let filterData;
+//     let inputData = e.target.value
+//     console.log(inputData);
+//     filterData = data.filter((item) => {
+//         return item.login.includes(inputData);
+//     })
+//     displayData(filterData);
+// })
