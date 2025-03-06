a08db1b5f200407fd56b4349dab11523e0b98699
Suraj Kumar <suraj28j@gmail.com>
2025-03-06 16:47:16 +0530

    update task2

--- Task/task2.js
+++ Task/task2.js
@@ -44,7 +44,7 @@ function toTitleCase(str) {
     let ary = str.split(" ");
     let temp = []
     for (let i = 0; i < ary.length; i++) {
-        temp.push(ary[i].replace(/^[a-z]/g,(x)=>x.toUpperCase()))
+        temp.push(ary[i].replace(/^[a-z]/g, (x) => x.toUpperCase()))
     }
     return temp.join(" ");
 }
@@ -105,22 +105,22 @@ function letterMatch(str1, str2) {
 // But for multiples of three print "Fizz" instead of the number and for the multiples of five print "Buzz". 
 // For numbers which are multiples of both three and five print "FizzBuzz".
 
-function fizzBuzz(){
-    for(let i = 1;i<=100;i++){
-        if(i%3===0 && i%5===0){
-            console.log("FizzBuzz ",i);
-        }else{
-            if(i%3===0)
-                console.log("Fizz ",i);
-            else{
-                if(i%5===0)
-                    console.log("Buzz ",i);
+function fizzBuzz() {
+    for (let i = 1; i <= 100; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log("FizzBuzz ", i);
+        } else {
+            if (i % 3 === 0)
+                console.log("Fizz ", i);
+            else {
+                if (i % 5 === 0)
+                    console.log("Buzz ", i);
             }
         }
     }
 }
 
-fizzBuzz()
+// fizzBuzz()
 
 // [9] Write a function that takes an array of objects and a key, and ...
 // returns a new array sorted based on the values of that key in ascending order.
@@ -142,4 +142,86 @@ function countWords(arr) {
     }
     return obj
 }
-console.log(countWords(["apple", "banana", "apple", "orange", "banana", "banana"])); // { apple: 2, banana: 3, orange: 1 }
+// console.log(countWords(["apple", "banana", "apple", "orange", "banana", "banana"])); // { apple: 2, banana: 3, orange: 1 }
+
+
+// [11] in object, find common properties who's key and value are same
+
+let obj1 = { a: 1, b: 2, c: 3, d: 10, e: 12 }
+let obj2 = { a: 2, e: 12, f: 6, d: 10 }
+
+let key1 = Object.keys(obj1)
+let key2 = Object.keys(obj2)
+let keys = []
+
+for (let i = 0; i < key1.length; i++) {
+    for (let j = 0; j < key2.length; j++) {
+        if (key1[i] === key2[j])
+            keys.push(key1[i]);
+    }
+}
+// console.log(keys);
+
+let res = {}
+for (let i = 0; i < keys.length; i++) {
+    if (obj1[`${keys[i]}`] === obj2[`${keys[i]}`]) {
+        res[`${keys[i]}`] = obj1[`${keys[i]}`]
+    }
+}
+
+// console.log(res);
+
+
+// [12] remove duplicates in given array
+
+const removeDuplicate = (ary) => {
+    let isDuplicate = false;
+    let res = [];
+    res.push(ary[0]);
+    for (let i = 0; i < ary.length; i++) {
+        for (let j = 0; j < res.length; j++) {
+            if (ary[i] === res[j])
+                isDuplicate = true;
+        }
+        if (!isDuplicate)
+            res.push(ary[i]);
+
+        isDuplicate = false;
+    }
+    console.log(res);
+}
+// removeDuplicate([1, 3, 5, 3, 8, 5])
+
+
+// [13] write a function to check if two given strings are anagrams of each other
+// example :- a = 'listen'; b = 'silent'
+
+const isAnagrams = (str1, str2) => {
+    let ary1 = str1.toLowerCase().split("").sort();
+    let ary2 = str2.toLowerCase().split("").sort();
+    for (let i = 0; i < str1.length; i++) {
+        if (ary1[i] !== ary2[i])
+            return false;
+    }
+    return true;
+}
+console.log(isAnagrams("listen","silent"));
+
+
+// [14] how many vowels in given string 
+
+const str = "Hey JS! You R AMAZING";
+const vowels = ['a', 'e', 'i', 'o', 'u'];
+
+const countVolwels = (data) => {
+    let count = 0;
+    data.toLowerCase().split("").forEach((ch) => {
+
+        // if (vowels.includes(ch))
+        //     count++;
+
+        vowels.includes(ch) && count++;
+    })
+    return count;
+}
+// console.log(countVolwels(str));
